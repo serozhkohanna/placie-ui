b1e0baac1d3df8acdd08944c07831f1a9a7bf3c4
serozhkohanna <serozhkohanna@gmail.com>
2021-12-08 16:20:48 +0200

    Added rules to prettier/eslint files.

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+
+import classNames from 'classnames';
+
+import styles from './Heading.scss';
+
+interface Props {
+  level: number;
+  text: string;
+}
+
+const Heading: FC<Props> = ({ level, text }) => {
+  const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+
+  return <Tag className={classNames(styles.heading, styles.h)}>{text}</Tag>;
+};
+
+export default Heading;
