cd157cfc431165c98e2fbbc4e86ff8e5512f3a28
Hanna Serozhko <33734821+serozhkohanna@users.noreply.github.com>
2021-12-13 15:11:20 +0200

    UI-KIT  (#10)
    * Setted variables for new colors and typography.
    
    * Refactored file. Move to variables.
    
    * Refactored breakpoints.
    
    * Button components.
    
    * Input component
    
    * V.1.0.3

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,14 +2,36 @@ import React from 'react';
 
 import './index.css';
 
-import { Button, Heading } from 'placie-ui';
+import { Button, Heading, Input } from 'placie-ui';
 
 const App = () => {
+  const handleClick = () => {
+    console.log('click');
+  };
+
   return <>
-    <Button />
+    <Button
+      text="BUTTON"
+      level={1}
+      disabled={false}
+      onClick={handleClick}
+    />
     <Heading
       text="Title text"
       level={1}
+    />
+    <Heading
+      text="Title text second"
+      level={2}
+    />
+    <Input
+      label="Enter place"
+      type="text"
+      value="text text"
+      placeholder="please enter smth"
+      name="field-input"
+      isRequired
+      onChange={() => console.log('text')}
     />
   </>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import Button from './components/Button/Button';
 import Heading from './components/Heading/Heading';
+import Input from './components/Input/Input';
 
-export { Button, Heading };
+export * from '../src/styles/reset.scss';
+
+export { Button, Heading, Input };
