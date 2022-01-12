de39ed3554f76e575ade2aadddb3b0e1b9c87aa6
Hanna Serozhko <33734821+serozhkohanna@users.noreply.github.com>
2022-01-12 17:06:43 +0200

    UI KIT (#16)
    * Fix heart button (change cursor).
    
    * V1.0.7
    
    * Level 4 button.
    
    * Level 3 button.
    
    * v.1.0.8

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,17 +6,21 @@ import styles from './Button.module.scss';
 
 interface Props {
   level: number;
-  text: string;
   disabled?: boolean;
   onClick: () => void;
+  children?: React.ReactNode;
+  text: string;
 }
 
-const Button: FC<Props> = ({ onClick, level, text, disabled }: Props) => {
+const Button: FC<Props> = ({ onClick, level, disabled, children, text }: Props) => {
   return <button
     onClick={onClick}
     disabled={disabled}
     className={classNames(styles.button, styles[`button-l${level}`])}
-  >{text}</button>;
+  >
+    {text}
+    <span className={styles.icon}>{children}</span>
+  </button>;
 };
 
 export default Button;
